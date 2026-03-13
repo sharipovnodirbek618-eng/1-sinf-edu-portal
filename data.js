e364097220aa1618a641a598ce3188851c04f21d
sharipovnodirbek618-eng <sharipovnodirbek618@gmail.com>
2026-03-13 12:49:37 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+const schoolData = {
+  "Toshkent shahri": {
+    "Yunusobod tumani": ["45-sonli maktab", "58-sonli maktab", "81-sonli maktab"],
+    "Chilonzor tumani": ["120-sonli maktab", "173-sonli maktab"]
+  },
+  "Farg'ona viloyati": {
+    "So'x tumani": ["30-sonli maktab", "17-sonli maktab"],
+    "Qo'qon shahri": ["5-sonli maktab", "22-sonli maktab"]
+  },
+  "Samarqand viloyati": {
+    "Samarqand shahri": ["12-sonli maktab", "33-sonli maktab"],
+    "Urgut tumani": ["7-sonli maktab", "14-sonli maktab"]
+  },
+  "Andijon viloyati": {
+    "Andijon shahri": ["1-sonli maktab", "15-sonli maktab"],
+    "Asaka tumani": ["9-sonli maktab", "27-sonli maktab"]
+  }
+};
